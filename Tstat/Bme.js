1be83230dc8668eeab42ea4ccf043b61dad0fa60
Judson Hartley <judson.hartley2@gmail.com>
2023-02-06 11:36:23 -0600

    filter

--- Tstat/Bme.js
+++ Tstat/Bme.js
@@ -5,6 +5,7 @@ function Bme(opts, name, rate, tempCorrection = 0, pressureCorrection = 0, humid
     this.options = opts;
     this.name = name;
     this.rate = rate;
+    this.lastTime = 0;
     this.bmeObj = new BME280(this.options);
     this.temperatureCorrection = tempCorrection;
     this.pressureCorrection = pressureCorrection;
@@ -24,13 +25,19 @@ function Bme(opts, name, rate, tempCorrection = 0, pressureCorrection = 0, humid
 Bme.prototype.read = async function() {
     try {
         let data = await this.bmeObj.readSensorData();
+        let now = Date.now();
+        let tdiff = now-this.lastTime;
+        this.lastTime = now;
 	console.log(data);
         this.temperature = parseFloat(data.temperature_C)*1.8+32+this.temperatureCorrection;
         this.temperature = this.temperature.toFixed(3);
+        if (Math.abs(this.temperature-this.temperatureOld) > 100 && tdiff < 30000) this.temperature = this.temperatureOld;
         this.pressure = parseFloat(data.pressure_hPa) + this.pressureCorrection;
         this.pressure = this.pressure.toFixed(3);
+        if (Math.abs(this.pressure-this.pressureOld) > 20 && tdiff < 30000) this.pressure = this.pressureOld;
         this.humidity = parseFloat(data.humidity) + this.humidityCorrection;
         this.humidity = this.humidity.toFixed(3);
+        if (Math.abs(this.humidity-this.humidityOld) > 20 && tdiff < 30000) this.humidity = this.humidityOld;
     } catch (err) { console.error(err); }
     this.publish();
 }
