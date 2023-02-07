0de7cbf9c54014e6cfc32e9de58b61afad462d55
Judson Hartley <judson.hartley2@gmail.com>
2023-02-07 15:34:38 -0600

    finish Ds18_v2 rewrite

--- Tstat/Ds18_v2.js
+++ Tstat/Ds18_v2.js
@@ -4,7 +4,6 @@ const DS18B20 = require('ds2482-temperature');
 function DsTs(rate) {
     this.rate = rate/1000;
     this.sense = new DS18B20({pollRate: this.rate});
-    this.currentSensors = [];
     this.dataStore = {};
     this.sense.init().
     then(() => {
@@ -20,47 +19,41 @@ DsTs.prototype.errorHandler = async function(err, where = 'unknown') {
     console.log(`Resetting DsTs`);
     try {
         await this.sense.destroy();
+        this.sense = new DS18B20({pollRate: this.rate});
         await this.sense.init();
         this.sense.on('data', val => { console.log(val); console.log('onData'); });
         this.sense.on('error', err => { this.errorHandler(err, "onError");});
     } catch (err) {this.errorHandler(err, "ErrorHandler");}
 }
 
-DsTs.prototype.read = async function() {
+DsTs.prototype.read = async function(data) {
     try {
-        let data = await this.sense.readTemperatures();
-	    console.log(data);
-        for (i = 0; i < data.length; i++) {
-            if (this.dataStore?.[data[i].rom] === undefined) {
-                this.currentSensors.push(data[i].rom);
-                this.dataStore[data[i].rom] = {
-                    temperature: 0,
-                    temperatureOld: 0
-                }
+        if (this.dataStore?.[data.rom] === undefined) {
+            this.dataStore[data.rom] = {
+                temperature: 0,
+                temperatureOld: 0
             }
-            this.dataStore[data[i].rom].temperature = (data[i].value*1.8+32).toFixed(2);
         }
+        this.dataStore[data.rom].temperature = (data.value*1.8+32).toFixed(2);
     } catch (err) {this.errorHandler(err, "DsTs.read");}
-    return this.publish();
+    return this.publish(data.rom);
 }
 
-DsTs.prototype.publish = function() {
-    for (i = 0; i < this.currentSensors.length; i++) {
-        if (this.dataStore?.[this.currentSensors[i]] === undefined) continue;
-        let { temperature, temperatureOld} = this.dataStore[this.currentSensors[i]];
-        if (Math.abs(temperature - temperatureOld) > 0.2) {
-            client.publish(`home/hvac/DS18B20/${this.currentSensors[i]}/temperature`, temperature.toString());
-            temperatureOld = temperature;
-        }
+DsTs.prototype.publish = function(rom) {
+    if (this.dataStore?.[rom] === undefined) return;
+    let { temperature, temperatureOld} = this.dataStore[rom];
+    if (Math.abs(temperature - temperatureOld) > 0.2) {
+        client.publish(`home/hvac/DS18B20/${rom}/temperature`, temperature.toString());
+        temperatureOld = temperature;
     }
 }
 
 DsTs.prototype.resend = function() {
-    for (i = 0; i < this.currentSensors.length; i++) {
-        if (this.dataStore?.[this.currentSensors[i]] === undefined) continue;
-        let { temperature } = this.dataStore[this.currentSensors[i]];
+    for (let rom in this.dataStore) {
+        if (this.dataStore?.[rom] === undefined) continue;
+        let { temperature } = this.dataStore[rom];
         if (temperature === 0) continue;
-        client.publish(`home/hvac/DS18B20/${this.currentSensors[i]}/temperature`, temperature.toString());
+        client.publish(`home/hvac/DS18B20/${rom}/temperature`, temperature.toString());
     }
 }
 
