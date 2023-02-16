2035e226de31004ec68951d2dc637ac5f3f2b86c
Judson Hartley <judson.hartley2@gmail.com>
2023-02-16 12:04:09 -0600

    test

--- Tstat/hvacBuilder.js
+++ Tstat/hvacBuilder.js
@@ -1,6 +1,6 @@
 
 const SimMachine = require("./machines/simMachine.js");
-const GpipMachine = require("./machines/gpioMachine.js");
+const GpioMachine = require("./machines/gpioMachine.js");
 const Machine = require('./machines/machine_v3.js');
 
 const Bme = require("./sensors/Bme_v3.js");
@@ -11,7 +11,7 @@ const Serial = require("./sensors/Serial_v3.js");
 class hvacBuilder {
     constructor(options) {
         const hvacClasses = {
-            SimMachine, Machine, Bme, DsTs, Serial, GpipMachine,
+            SimMachine, Machine, Bme, DsTs, Serial, GpioMachine,
         };
         console.log(options);
         return new hvacClasses[options.neededClass](options);
