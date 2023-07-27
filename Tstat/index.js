b401f1f50de35da1d901f13cd599c302f01dce96
Judson Hartley <52391697+jahartley@users.noreply.github.com>
2023-07-27 17:38:32 -0600

    Update index.js

--- Tstat/index.js
+++ Tstat/index.js
@@ -14,6 +14,7 @@
 */
 
 
+
 console.log("-------------------------------------------------");
 const {client, pigpio, dataBus} = require("./global.js");
 const hvacLogic = require("./hvacLogic.js");
