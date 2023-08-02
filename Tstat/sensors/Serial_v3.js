f66c48fef22b30e01741405047434ef0fd96eb16
Judson Hartley <52391697+jahartley@users.noreply.github.com>
2023-08-02 11:38:14 -0600

    Update Serial_v3.js

--- Tstat/sensors/Serial_v3.js
+++ Tstat/sensors/Serial_v3.js
@@ -14,8 +14,9 @@ class Serial extends Sensor {
             this.port = new SerialPort({ path: '/dev/serial0', baudRate: 9600});
             this.parser = this.port.pipe(new DelimiterParser({ delimiter: '\n' }));
             this.parser.on('data', (data) => {
-                let newData = data.toString();
-                let header = newData.slice(0,1);
+                const newData = data.toString();
+                console.log(newData);
+                const header = newData.slice(0,1);
                 /* 
                     t70.25
                     y70.1
@@ -43,12 +44,12 @@ class Serial extends Sensor {
                         break;
                     case 'T':
                         let timeString = newData.slice(5, newData.length-1);
-                        let timeString2 = timeString.split(':');
-                        if (timeString2.length != 3) break;
+                        timeString = timeString.split(':');
+                        if (timeString.length != 3) break;
                         let date2 = new Date();
-                        if (timeString2[0] != date2.getHours()) this.port.write(`o${date2.getHours()}\r\n`);
-                        if (timeString2[1] != date2.getMinutes()) this.port.write(`m${date2.getMinutes()}\r\n`);
-                        if (timeString2[2] != date2.getSeconds()) this.port.write(`s${date2.getSeconds()}\r\n`);
+                        if (timeString[0] != date2.getHours()) this.port.write(`o${date2.getHours()}\r\n`);
+                        if (timeString[1] != date2.getMinutes()) this.port.write(`m${date2.getMinutes()}\r\n`);
+                        if (timeString[2] != date2.getSeconds()) this.port.write(`s${date2.getSeconds()}\r\n`);
                 }
             });
             console.log("Serial initialized");
